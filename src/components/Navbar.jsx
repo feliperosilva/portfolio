c8bc32a916734b168972cab3bf6ecb5d5c147725
Felipe Silva <felipe.silva@webtech87.pt>
2025-04-23 12:29:05 +0100

    [UPDATE] Responsiveness in mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,58 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from "../styles/Navbar.module.css";
 import logo from "../assets/Felipe-designstyle-cartoon-m.png"
 
 const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
-        <Link to="/#home">
-            <img src={logo} alt="logo" className={styles.logo}/>
-        </Link>
-        <ul className={styles.links}>
-            <li>
-                <Link to="/#about" className={styles.animated_link}>About</Link>
-            </li>      
-            <li>
-                <Link to="/#products" className={styles.animated_link}>Products</Link>
-            </li>      
-            <li>
-                <Link to="/#courses" className={styles.animated_link}>Courses</Link>
-            </li>      
-            <li>
-                <Link to="/#contact" className={styles.animated_link}>Contact</Link>
-            </li>      
-        </ul>
-        <div className={styles.nav_buttons}>
-            <button className='cta'>
-                Login
-            </button>        
-            <button className='cta'>
-                Sign Up
-            </button> 
-        </div>
-               
-    </nav>
-    
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => setIsOpen(!isOpen);
+    const closeMenu = () => setIsOpen(false) 
+
+    return (
+        <nav className={styles.navbar}>
+
+            <Link to="/#home">
+                <img src={logo} alt="logo" className={styles.logo}/>
+            </Link>
+            <div className={styles.hamburger} onClick={toggleMenu}>
+                ☰
+            </div>
+            <ul className={`${styles.links} ${isOpen ? styles.menu_open : ''}`}>
+                <li>
+                    <Link to="/#about" className={styles.animated_link} onClick={closeMenu}>About</Link>
+                </li>      
+                <li>
+                    <Link to="/#products" className={styles.animated_link} onClick={closeMenu}>Products</Link>
+                </li>      
+                <li>
+                    <Link to="/#courses" className={styles.animated_link} onClick={closeMenu}>Courses</Link>
+                </li>      
+                <li>
+                    <Link to="/#contact" className={styles.animated_link} onClick={closeMenu}>Contact</Link>
+                </li>      
+                <li>
+                    <button className={styles.button_mobile}>
+                        Login
+                    </button>
+                </li>      
+                <li>
+                    <button className={styles.button_mobile}>
+                        Sign Up
+                    </button>
+                </li>      
+            </ul>
+            <div className={styles.nav_buttons}>
+                <button className='cta'>
+                    Login
+                </button>        
+                <button className='cta'>
+                    Sign Up
+                </button> 
+            </div>           
+        
+                 
+        </nav>    
   )
 }
 
